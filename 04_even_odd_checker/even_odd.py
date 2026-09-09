6b07abdb9c36dc8552d9da5c05921f55915a7e79
Jayshri Borase <jayshriborase25aiml04@gmail.com>
2026-09-09 11:26:32 +0530

    Create even_odd.py

---
+++ 04_even_odd_checker/even_odd.py
@@ -0,0 +1,8 @@
+print("===== Even or Odd Checker =====")
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print(number, "is an Even number.")
+else:
+    print(number, "is an Odd number.")
